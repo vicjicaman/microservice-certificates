bdc376609f276fb97209e3521669d4a4daa5164f
Victor Jimenez <vic.jicama@gmail.com>
2019-09-30 23:04:01 +0200

    Group kubernetes entities per service/stateful/cronjob

--- src/index.js
+++ src/index.js
@@ -3,30 +3,52 @@ const express = require("express");
 import { execSync } from "child_process";
 import * as Utils from "@nebulario/microservice-utils";
 
-const DOMAIN = "repoflow.com";
-const EMAIL = "vic.jicama@gmail.com";
 
-console.log("Check certificates for " + DOMAIN);
-const stdout = execSync(`certbot certificates -d ${DOMAIN} -d *.${DOMAIN} `);
+const NAMESPACE = process.env["NAMESPACE"];
+const DOMAIN = process.env["DOMAIN"];
+const EMAIL = process.env["EMAIL"];
+const ENV_TYPE = process.env["ENV_TYPE"];
+
+const LOGS = "/certificates/var/log/letsencrypt";
+const CONFIG = "/certificates/etc/letsencrypt";
+const LIB = "/certificates/var/lib/letsencrypt";
+
+console.log("Check certificates for " + DOMAIN + " with " + EMAIL);
+
+const stdout = execSync(
+  `certbot certificates -d ${DOMAIN} -d *.${DOMAIN} --logs-dir ${LOGS} --config-dir ${CONFIG} --work-dir ${LIB} `
+);
 const validRegEx = /VALID:\s+(\d+)\s+days/g;
 
 const res = validRegEx.exec(stdout);
 if (res) {
   const [match, daysStr] = res;
   const days = parseInt(daysStr);
-  console.log(DOMAIN + " certificates are valid " + days + " days");
+  console.log(DOMAIN + " certificates are valid " + days + " days...");
 
   if (days < 12) {
     console.log("Renew certificates");
-    //const stdout = execSync(`certbot renew --dry-run  --dns-route53 --non-interactive `);
-    //console.log(stdout.toString());
+    const stdout = execSync(
+      `certbot renew  ` +
+        (ENV_TYPE === "DEV" ? "--dry-run" : "") +
+        `  --dns-route53 --non-interactive  --logs-dir ${LOGS} --config-dir ${CONFIG} --work-dir ${LIB} `
+    );
+    console.log(stdout.toString());
   }
+
+  const stdout = execSync(
+    ` curl -ik -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" https://kubernetes.default.svc.cluster.local/api/v1/namespaces/${NAMESPACE}/pods `
+  );
+  console.log(stdout.toString());
+
 } else {
-  console.log("Create certificates");
-  //const stdout = execSync(`certbot certonly -d ${DOMAIN} -d *.${DOMAIN}  --dns-route53 -m ${EMAIL} --agree-tos --non-interactive`);
-  //console.log(stdout.toString());
+  console.log("Create certificates.");
+  const stdout = execSync(
+    `certbot certonly ` +
+      (ENV_TYPE === "DEV" ? "--dry-run" : "") +
+      ` -d ${DOMAIN} -d *.${DOMAIN}  --dns-route53 -m ${EMAIL} --agree-tos --non-interactive --logs-dir ${LOGS} --config-dir ${CONFIG} --work-dir ${LIB}`
+  );
+  console.log(stdout.toString());
 }
-
-
 
 Utils.Process.shutdown(signal => console.log("Shutdown with " + signal));
